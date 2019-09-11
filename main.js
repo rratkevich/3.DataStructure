f2f790606000f9e6635339eecfac9d9e15041c66
rratkevich <ratkevichigor94@gmail.com>
2019-09-11 12:46:22 +0300

    tree was changed

--- main.js
+++ main.js
@@ -33,62 +33,192 @@ console.log(el2);
 
 // create binary tree
 
-let binaryTree = {
-    value: 43,
-    left: null,
-    right: null
-};
+// node
+class Node { 
+    constructor(data) 
+    { 
+        this.data = data; 
+        this.left = null; 
+        this.right = null; 
+    } 
+}; 
 
-console.log(binaryTree);
+console.log(Node);
 
-function Node(data) {
-    this.data = data;
-    this.parent = null;
-    this.children = [];
-};
+// tree
+class BinarySearchTree 
+{ 
+    constructor() 
+    { 
+        this.root = null; 
+    } 
+   
+ // создание нового узла   
+    insert(data) 
+    { 
 
-function Tree(data) {
-    let node = new Node(data);
-    this._root = node;
-};
+        let newNode = new Node(data); 
+                        
+        if (this.root === null) 
+            this.root = newNode; 
+        else
+            this.insertNode(this.root, newNode); 
+            
+    }  
 
-let tree = new Tree('CEO');
-tree._root;
-console.log(tree._root);
+// вставка узла в дерево на свое место в зависимости от значения
+    insertNode(node, newNode) 
+    { 
 
-Tree.prototype.traverseDF = function(callback) {
+        if(newNode.data < node.data) { 
+            if(node.left === null) 
+                node.left = newNode; 
+            else
 
-    (function recurse(currentNode) {
-        for (let i = 0, length = currentNode.children.length; i < length; i++) {
-            recurse(currentNode.children[i]);
-        }
+                this.insertNode(node.left, newNode); 
+        } 
 
-        callback(currentNode);
-        
-    })(this._root);
+        else { 
+            if(node.right === null) 
+                node.right = newNode; 
+            else
 
-};
+                this.insertNode(node.right,newNode); 
+        } 
+    } 
 
-let tree1 = new Tree('one');
+    // удаление узла
+    remove(data) 
+    { 
+        this.root = this.removeNode(this.root, data); 
+    } 
+      
+    removeNode(node, key)  
+    { 
+        if(node === null) 
+            return null; 
+    
+        else if(key < node.data) 
+        { 
+            node.left = this.removeNode(node.left, key); 
+            return node; 
+        } 
+    
+        else if(key > node.data) { 
+            node.right = this.removeNode(node.right, key); 
+            return node; 
+        } 
+    
+        else { 
+            if(node.left === null && node.right === null) { 
+                node = null; 
+                return node; 
+            } 
+    
+            if(node.left === null) { 
+                node = node.right; 
+                return node; 
+            } 
+              
+            else if(node.right === null) { 
+                node = node.left; 
+                return node; 
+            } 
+    
+            let aux = this.findMinNode(node.right); 
+            node.data = aux.data; 
+      
+            node.right = this.removeNode(node.right, aux.data); 
+            return node; 
+        } 
+      }  
+                    
+    // Методы
+    // поиск узла с минимальным значением
 
-tree._root.children.push(new Node('two'));
-tree._root.children[0].parent = tree;
+    findMinNode(node) 
+    { 
 
-tree._root.children.push(new Node('three'));
-tree._root.children[1].parent = tree;
+        if(node.left === null) 
+            return node; 
+        else
+            return this.findMinNode(node.left); 
+    }
 
-tree._root.children.push(new Node('four'));
-tree._root.children[2].parent = tree;
+        // корневой узел
+    getRootNode() 
+    { 
+        return this.root; 
+    } 
 
-tree._root.children[0].children.push(new Node('five'));
-tree._root.children[0].children[0].parent = tree._root.children[0];
+    // обход дерева
 
-tree._root.children[0].children.push(new Node('six'));
-tree._root.children[0].children[1].parent = tree._root.children[0];
+    // inorder
 
-tree._root.children[2].children.push(new Node('seven'));
-tree._root.children[2].children[0].parent = tree._root.children[2];
+    inorder(node) 
+    { 
+        if(node !== null) { 
+            this.inorder(node.left); 
+            console.log(node.data); 
+            this.inorder(node.right); 
+        } 
+    } 
+    
+    // preorder
+    
+    preorder(node) 
+    { 
+        if(node != null) { 
+            console.log(node.data); 
+            this.preorder(node.left); 
+            this.preorder(node.right); 
+        } 
+    } 
+    
+    // postorder
+    
+    postorder(node) 
+    { 
+        if(node != null) { 
+            this.postorder(node.left); 
+            this.postorder(node.right); 
+            console.log(node.data); 
+        } 
+    } 
 
-tree.traverseDF(function(node) {
-    console.log(node.data)
-});
+        // поиск узла с определенным значением
+    search(node, data) 
+    { 
+
+        if(node === null) 
+            return null; 
+    
+        else if(data < node.data) 
+            return this.search(node.left, data); 
+    
+        else if(data > node.data) 
+            return this.search(node.right, data); 
+    
+        else
+            return node; 
+    } 
+} 
+
+// реализация
+
+let BST = new BinarySearchTree(); 
+
+BST.insert(32); 
+BST.insert(4); 
+BST.insert(54); 
+BST.insert(3); 
+BST.insert(2); 
+
+console.log(BST);
+
+let root = BST.getRootNode(); 
+
+BST.inorder(root); 
+
+BST.remove(4); 
+BST.inorder(root); 
